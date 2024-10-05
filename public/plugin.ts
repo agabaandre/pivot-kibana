5c131a682c2280757cfdeddab9482c2d572fe58a
Agaba Andre <agabaandre@gmail.com>
2024-10-05 03:00:05 +0300

    Update plugin.ts

--- public/plugin.ts
+++ public/plugin.ts
@@ -1,1 +1,30 @@
+import { AppMountParameters, CoreSetup, CoreStart, Plugin } from '../../../src/core/public';
+import { i18n } from '@kbn/i18n';
+import { PLUGIN_NAME } from '../common';
+import { FlexmonsterPivotTable } from './components/FlexmonsterPivotTable';
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+
+export class FlexmonsterPivotPlugin implements Plugin {
+  public setup(core: CoreSetup) {
+    core.application.register({
+      id: 'flexmonsterPivot',
+      title: i18n.translate('flexmonsterPivot.pluginTitle', {
+        defaultMessage: 'Flexmonster Pivot',
+      }),
+      mount: async (params: AppMountParameters) => {
+        const [coreStart] = await core.getStartServices();
+        const { element } = params;
+
+        render(<FlexmonsterPivotTable esClient={coreStart.http} />, element);
+
+        return () => unmountComponentAtNode(element);
+      },
+    });
+  }
+
+  public start(core: CoreStart) {}
+
+  public stop() {}
+}
 
